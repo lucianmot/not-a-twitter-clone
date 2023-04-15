86fb7be6524751828bf166bf73a4876477aef0ef
Lucian Mot <lucian.i.mot@gmail.com>
2023-04-15 21:31:03 +0100

    storing this for another project

---
+++ src/utils/types.ts
@@ -0,0 +1,81 @@
+// export type Player = {
+//   id: string;
+//   name: string;
+//   description: string;
+//   level: number;
+//   xp: number;
+//   levelUpXp: number;
+//   abilities: Ability[];
+//   abilitySlotOne: Ability;
+//   abilitySlotTwo: Ability;
+//   abilitySlotThree: Ability;
+//   ultimateAbilitySlot: UltimateAbility;
+//   inventory: Item[];
+// };
+
+// export type Item = {
+//   id: string;
+//   name: string;
+//   description: string;
+// };
+
+// export type Ability = {
+//   id: string;
+//   name: string;
+//   description: string;
+//   level: number;
+//   xp: number;
+// };
+
+// export interface UltimateAbility extends Ability {
+//   id: string;
+//   name: string;
+//   description: string;
+//   level: number;
+//   xp: number;
+// }
+
+// export type Dungeon = {
+//   id: string;
+//   name: string;
+//   description: string;
+//   monster: Monster;
+//   Boss: Boss;
+// };
+
+// export type DungeonType = {
+//   id: string;
+//   name: string;
+//   description: string;
+//   type:
+//     | "Skeleton"
+//     | "Spider"
+//     | "Zombie"
+//     | "Goblin"
+//     | "Orc"
+//     | "Troll"
+//     | "Dragon"
+//     | "Demon"
+//     | "Undead";
+// };
+
+// export type Monster = {
+//   id: string;
+//   name: string;
+//   description: string;
+// };
+
+// export type Boss = {
+//   id: string;
+//   name: string;
+//   description: string;
+// };
+
+// export type ActionItem = {
+//   id: string;
+//   name: string;
+//   description: string;
+//   estimatedTime: number;
+//   deadline: Date;
+//   completed: boolean;
+// };
